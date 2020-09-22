dc003e6e7e51fd1c8cccc4d89c108573b9ba0e78
jalon724 <427nolaj>
2020-09-22 15:45:20 +0800

    something changed

--- GitOne/src/main/java/Userscontroller.java
+++ GitOne/src/main/java/Userscontroller.java
@@ -7,6 +7,9 @@ public class Userscontroller {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
             System.out.println("i = " + i);
+            if(i>90){
+                break;
+            }
         }
         System.out.println("git");
         System.out.println("加到本地仓库嗷嗷嗷");
