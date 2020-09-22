39f2d4df4b3e87b5f860f62a81b66f2734b63f4e
jalon724 <427nolaj>
2020-09-22 14:08:46 +0800

    add a svnone to git

---
+++ svnone/src/main/java/Student.java
@@ -0,0 +1,14 @@
+/**
+ * @author jalon
+ * @date 2020-9-19 10:56
+ */
+public class Student {
+
+    String name;
+    int age;
+    String tell;
+
+    public static void main(String[] args) {
+        System.out.println("hahaha");
+    }
+}
